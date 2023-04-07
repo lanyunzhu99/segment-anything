fc19cd4c568a711affb10daf19ac8b152697ec59
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-07 20:27:46 +0800

    .

--- test_prompy.py
+++ test_prompy.py
@@ -1,5 +1,7 @@
-
+import cv2
 from segment_anything import build_sam, SamPredictor 
 predictor = SamPredictor(build_sam(checkpoint="/home/lanyunz/sam/sam_vit_h_4b8939.pth"))
-predictor.set_image("/home/lanyunz/sam_2/segment-anything/test_image/2.jpg")
+image = cv2.imread("/home/lanyunz/sam_2/segment-anything/test_image/2.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+predictor.set_image(image)
 masks, _, _ = predictor.predict()
