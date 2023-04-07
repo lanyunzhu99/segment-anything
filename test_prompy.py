1a6b90e7731516829be5c0428a826bf883c32244
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-07 20:07:29 +0800

    .

--- test_prompy.py
+++ test_prompy.py
@@ -1,3 +1,4 @@
+
 from segment_anything import build_sam, SamPredictor 
 predictor = SamPredictor(build_sam(checkpoint="/home/lanyunz/sam/sam_vit_h_4b8939.pth"))
 predictor.set_image("/home/lanyunz/sam_2/segment-anything/test_image/1.jpg")
