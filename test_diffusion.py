d8954c74c90a8a15b539102cead0662cdeaa9648
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-07 20:53:38 +0800

    .

--- test_diffusion.py
+++ test_diffusion.py
@@ -8,7 +8,7 @@ device = "cuda"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to(device)
 
-prompt = "an image of a girl playing guitar in the woods by the creek"
+prompt = "A girl is playing her guitar in the woods by the creek, a jeep is parked next to the girl"
 image = pipe(prompt).images[0]  
     
-image.save("./diffusion_generate.png")
+image.save("./diffusion_generate_2.png")
