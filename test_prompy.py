4d2851599ce38b44cdc1738c5982f842349b03df
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-07 20:03:01 +0800

    .

--- test_prompy.py
+++ test_prompy.py
@@ -1,4 +1,4 @@
 from segment_anything import build_sam, SamPredictor 
-predictor = SamPredictor(build_sam(checkpoint="/home/lanyunz/samsam_vit_h_4b8939.pth"))
+predictor = SamPredictor(build_sam(checkpoint="/home/lanyunz/sam/samsam_vit_h_4b8939.pth"))
 predictor.set_image("/home/lanyunz/sam_2/segment-anything/test_image/Slaty_Backed_Gull_0086_786387.jpg")
 masks, _, _ = predictor.predict("birds in the image")
