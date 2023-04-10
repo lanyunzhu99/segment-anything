ed0f544ea64675285cca5b0d0d6b823d6601e87e
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-11 01:14:46 +0800

    .

--- test_diffusion.py
+++ test_diffusion.py
@@ -11,6 +11,7 @@ pipe = pipe.to(device)
 prompt = "A picture of a rectangular carpet. This carpet is loved by little girls. This carpet is not a solid color. Its style is very loevely. There are patterns of small animals printed on the carpet. The color of the carpet is a light color with low saturation. The four edges of the carpet have border lines."
 prompt = "A carpet in Alice's Wonderland style. This carpet is very cute."
 prompt = "A photo of a japanese beautiful girl with large chest, oval face and large eyes. She wears JK uniform"
+prompt = "A photo of a handsome Chinese man with a chin line, high nose bridge, double eyelids, and a prominent Adam's apple."
 image = pipe(prompt).images[0]  
     
 image.save("./diffusion_generate_3.png")
