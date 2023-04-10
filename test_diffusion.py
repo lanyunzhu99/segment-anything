996e72e7b6c4e49d1e51019ae67085e18691a092
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-11 00:40:36 +0800

    .

--- test_diffusion.py
+++ test_diffusion.py
@@ -8,7 +8,7 @@ device = "cuda"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to(device)
 
-prompt = "A picture of a rectangular carpet. This carpet is not a solid color. Its style is very cute. There are patterns of small animals such as rabbits and cats printed on the carpet. The color of the carpet is a light color with low saturation. The four edges of the carpet have border lines."
+prompt = "A picture of a rectangular carpet. This carpet is not a solid color. Its style is very loevely. There are patterns of small animals printed on the carpet. The color of the carpet is a light color with low saturation. The four edges of the carpet have border lines."
 image = pipe(prompt).images[0]  
     
 image.save("./diffusion_generate_3.png")
