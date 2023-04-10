afab232213bfb45498534ec0332139b2ce975c2b
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-11 01:06:07 +0800

    .

--- test_diffusion.py
+++ test_diffusion.py
@@ -10,6 +10,7 @@ pipe = pipe.to(device)
 
 prompt = "A picture of a rectangular carpet. This carpet is loved by little girls. This carpet is not a solid color. Its style is very loevely. There are patterns of small animals printed on the carpet. The color of the carpet is a light color with low saturation. The four edges of the carpet have border lines."
 prompt = "A carpet in Alice's Wonderland style. This carpet is very cute."
+prompt = "A photo of a Chinese beautiful girl with large chest, oval face and large eyes. She wears Jk uniform"
 image = pipe(prompt).images[0]  
     
 image.save("./diffusion_generate_3.png")
