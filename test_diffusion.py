7dfd05cf1e9ed988cc69237e908fa861951221cb
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-11 00:25:24 +0800

    .

--- test_diffusion.py
+++ test_diffusion.py
@@ -8,7 +8,7 @@ device = "cuda"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to(device)
 
-prompt = "A picture of a rectangular rug. This rug is not a solid color, it has a cute style with small animal prints on the rug, the color of the rug is a lighter color with less saturation, and the four sides of the rug have border lines."
+prompt = "A picture of a rectangular rug. This rug is not a solid color, it has a cute style with small animal prints on the rug such as cat, rabbit, and bear, the color of the rug is a lighter color with less saturation, and the four sides of the rug have border lines."
 image = pipe(prompt).images[0]  
     
 image.save("./diffusion_generate_3.png")
