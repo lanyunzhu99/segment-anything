4a675b201ae433993974993ef0170d79420cab83
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-24 17:47:00 +0800

    .

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -142,7 +142,7 @@ class MaskDecoder(nn.Module):
         hyper_in = torch.stack(hyper_in_list, dim=1)
         b, c, h, w = upscaled_embedding.shape
         masks = (hyper_in @ upscaled_embedding.view(b, c, h * w)).view(b, -1, h, w)
-
+        print(masks.shape)
         # Generate mask quality predictions
         iou_pred = self.iou_prediction_head(iou_token_out)
 
