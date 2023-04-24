a652aae5cb0b98d1c7702c94b56c79ddf4eb14c8
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-24 18:53:52 +0800

    .

--- segment_anything_1/automatic_mask_generator.py
+++ segment_anything_1/automatic_mask_generator.py
@@ -240,7 +240,6 @@ class SamAutomaticMaskGenerator:
         points_for_image = self.point_grids[crop_layer_idx] * points_scale
 
         # Generate masks for this crop in batches
-        print('mmmmmm')
         data = MaskData()
         
         
@@ -253,7 +252,6 @@ class SamAutomaticMaskGenerator:
         
         
         for (points,) in batch_iterator(self.points_per_batch, points_for_image):
-            print(points.shape)
             batch_data = self._process_batch(points, cropped_im_size, crop_box, orig_size)
             data.cat(batch_data)
             del batch_data
