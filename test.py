932e3812ede0d06697c5371f5105a75485cc980d
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2023-04-26 13:17:58 +0800

    .

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ def main():
         outputs = sam([batch], multimask_output=False)
         masks = outputs[0]['masks']
         masks = torch.sigmoid(masks)
-        masks = masks.squeeze(0).squeeeze(0).cpu().numpy()
+        masks = masks.squeeze(0).squeeze(0).cpu().numpy()
         out = np.ones(masks.shape) * (masks > 0.5)
         out = np.uint8(out * 255)
         print(masks.shape)
